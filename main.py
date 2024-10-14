e594987bec8376430210904a19c93826bcbdf99d
Craig Giddings <randomhero_345@hotmail.com>
2024-10-14 17:24:59 -0400

    Added game grid

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 800
     SCREEN_BG = (51, 51, 51) 
+    TILE_SIZE = 20
     
     # Initialize pygame
     pygame.init()
@@ -19,6 +20,16 @@ def main():
 
     # Initialize pygame Clock
     clock = pygame.time.Clock()
+
+    # Create the game grid (may change in the future)
+    for row in range(SCREEN_HEIGHT // TILE_SIZE):
+        for col in range(SCREEN_WIDTH // TILE_SIZE):
+            pygame.draw.rect(
+                screen,
+                (178,34,34,250),
+                pygame.Rect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE),
+                1
+            )
 
     # main game loop
     while True:
