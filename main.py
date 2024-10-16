03c9db06400287600dbefeb88a0034942d800631
Craig Giddings <randomhero_345@hotmail.com>
2024-10-15 21:16:12 -0400

    Updated the way the grid is drawn and put it into its own function

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 import pygame 
 import sys
 
-
-def main():
-
-    # Screen constants
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 800
-    SCREEN_BG = (51, 51, 51) 
+            
+def draw_grid(screen):
+    """Draws the game grid to the screen"""
     TILE_SIZE = 20
-    
-    # Initialize pygame
-    pygame.init()
-
-    # Create the screen and set the title
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Snek")
-    screen.fill(SCREEN_BG)
-
-    # Initialize pygame Clock
-    clock = pygame.time.Clock()
-
-    # Create the game grid (may change in the future)
-    for row in range(SCREEN_HEIGHT // TILE_SIZE):
-        for col in range(SCREEN_WIDTH // TILE_SIZE):
+    for row in range(0, screen.get_width(), TILE_SIZE): 
+        for col in range(0, screen.get_height(), TILE_SIZE):
             pygame.draw.rect(
-                screen,
-                (178,34,34,250),
-                pygame.Rect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE),
+                screen, 
+                (51, 51, 51), 
+                pygame.Rect(row, col, TILE_SIZE, TILE_SIZE), 
                 1
             )
 
+
+def main():
+
+    # Constants
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 800
+    SCREEN_BG = (0, 0, 0) 
+    FPS = 60
+
+    # Initialize pygame
+    pygame.init()
+    # Initialize pygame Clock
+    clock = pygame.time.Clock()
+    
+    # Create the screen and set the title
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Snek")
+
+
     # main game loop
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
+
+        screen.fill(SCREEN_BG)
+
+        # Create the grid
+        draw_grid(screen)
+
         # Update screen
         pygame.display.flip()
 
-        # Set FPS to 144
-        clock.tick(144)
-
+        # Set framerate to 60 fps
+        clock.tick(FPS)
 
 if __name__ == "__main__":
     main()
