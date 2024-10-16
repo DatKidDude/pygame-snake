46efb01144df2dccea9f78f58108afaabeb5961b
Craig Giddings <randomhero_345@hotmail.com>
2024-10-15 21:29:05 -0400

    Added Snake class

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import pygame 
 import sys
 
-            
+
+class Snake:
+    """A snake that eats food on the screen"""
+
+    def __init__(self, screen, start_time):
+        """Initialize snake and screen instance"""
+        # Get the screen rect
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        
+        # Create snake
+        self.snake = pygame.Rect(400, 400, 20, 20) 
+        # Get (x, y) coordinates
+        self.last_time = start_time  
+        self.current_time = 0
+
+    
+    def draw_snake(self):
+        """Draw snake on the screen"""
+        pygame.draw.rect(self.screen, "green", self.snake)
+    
+
+    def move_snake(self):
+        """Move the snake across the screen every 100 milliseconds."""
+        self.current_time = pygame.time.get_ticks()
+        if self.current_time - self.last_time > 100:
+            self.snake.x += 20
+            self.last_time = self.current_time
+
+
+    def update(self):
+        """Update the snakes (x, y) position on the grid"""
+        self.move_snake()
+        self.draw_snake()
+        
+    
 def draw_grid(screen):
     """Draws the game grid to the screen"""
     TILE_SIZE = 20
@@ -27,11 +62,14 @@ def main():
     pygame.init()
     # Initialize pygame Clock
     clock = pygame.time.Clock()
+    # Number of ms since pygame.init() was called
+    start_time = pygame.time.get_ticks()
     
     # Create the screen and set the title
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Snek")
 
+    snake = Snake(screen, start_time)
 
     # main game loop
     while True:
@@ -45,6 +83,8 @@ def main():
 
         # Create the grid
         draw_grid(screen)
+
+        snake.update()
 
         # Update screen
         pygame.display.flip()
